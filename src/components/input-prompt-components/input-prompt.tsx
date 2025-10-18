c3d4adc6bfbdc3ec940f9636b618e17169ef52a5
MudasirSaeed032 <mudasir.saeed032@gmail.com>
2025-10-18 10:54:31 +0500

    Styling changes

--- src/components/input-prompt-components/input-prompt.tsx
+++ src/components/input-prompt-components/input-prompt.tsx
@@ -284,10 +284,10 @@ const InputPrompt = () => {
   };
 
   return (
-    <div className=" flex-shrink-0 w-full md:px-10 px-5 pb-2 space-y-2 bg-background">
+  <div className=" flex-shrink-0 w-full md:px-10 px-5 pb-2 space-y-2 -z-10 "style={{backgroundColor: 'transparent'}}>
       {inputImgName &&
         <div className="max-w-4xl overflow-hidden w-full mx-auto">
-          <div className="p-5 w-fit relative max-w-full overflow-hidden bg-card group rounded-t-3xl flex items-start gap-2">
+          <div className="p-5 w-fit relative max-w-full overflow-hidden bg-transparent group rounded-t-3xl flex items-start gap-2">
             <MdImageSearch className="text-4xl" />
             <p className="text-lg font-semibold truncate"> {inputImgName}</p>
             <IoMdClose onClick={() => { setInputImgName(null); setInputImg(null) }} className="absolute top-1 right-1 text-2xl rounded-full cursor-pointer hover:opacity-100 hidden group-hover:block opacity-80 bg-accentGray/40 p-1" />
@@ -295,7 +295,8 @@ const InputPrompt = () => {
         </div>
       }
       <div
-        className={`w-full md:border-[3px] border-4 relative border-transparent border-gradient max-w-4xl mx-auto min-h-16 md:rounded-[50px] rounded-2xl ${inputImgName && " !rounded-tl-none "} overflow-hidden flex gap-1 md:items-center md:justify-between md:flex-row flex-col`}
+        className={`w-full md:border-[3px] border-4 relative border-transparent border-gradient max-w-4xl mx-auto min-h-16 md:rounded-[50px] rounded-2xl ${inputImgName && " !rounded-tl-none "} overflow-hidden flex gap-1 md:items-center md:justify-between md:flex-row flex-col z-50`}
+        style={{position: 'relative'}}
       >
 
         {msgLoader ? (
